413f3adf9497e30269b6f4b92924b8f7e1c36574
Otabek Narz <otabeknarz@gmail.com>
2025-04-01 16:07:18 +0500

    commented for now

--- users/api/views.py
+++ users/api/views.py
@@ -3,8 +3,8 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 
-from .serializers import UserSerializer, SessionSerializer, OnboardingAnswersSerializer
-from users.models import User, OnboardingAnswers
+from .serializers import UserSerializer, SessionSerializer
+from users.models import User
 
 
 @api_view(["GET"])
@@ -65,13 +65,13 @@ def get_my_sessions(request):
     return Response(serializer.data, status=200)
 
 
-@api_view(["POST"])
-@permission_classes([IsAuthenticated])
-def create_onboarding_answers(request):
-    data = request.data
-    data.update(user=request.user.id)
-    serializer = OnboardingAnswersSerializer(data=data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=201)
-    return Response(serializer.errors, status=400)
+# @api_view(["POST"])
+# @permission_classes([IsAuthenticated])
+# def create_onboarding_answers(request):
+#     data = request.data
+#     data.update(user=request.user.id)
+#     serializer = OnboardingAnswersSerializer(data=data)
+#     if serializer.is_valid():
+#         serializer.save()
+#         return Response(serializer.data, status=201)
+#     return Response(serializer.errors, status=400)
