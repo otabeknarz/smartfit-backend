2078a06e3f81bbf6fcde9f614b10409befff3e3d
Otabek Narz <otabeknarz@gmail.com>
2025-05-07 14:12:39 +0500

    some changes for gunicorn

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,3 +1,5 @@
-accesslog = 'logs/gunicorn_access.log'
-errorlog = 'logs/gunicorn_error.log'
-loglevel = 'info'
+bind = "0.0.0.0:1234"
+workers = 5
+accesslog = "logs/gunicorn_access.log"
+errorlog = "logs/gunicorn_error.log"
+loglevel = "info"
