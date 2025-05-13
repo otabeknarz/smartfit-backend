34a3660bcca77670a041317aff73d8f92719ef4e
Otabek Narz <otabeknarz@gmail.com>
2025-05-14 01:41:19 +0500

    removed payme-pkg lib

--- payments/api/urls.py
+++ payments/api/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
-    path("payme/", views.PaymeCallBackAPIView.as_view()),
+    path("payme/", views.PaymeAPIView.as_view()),
 ]
